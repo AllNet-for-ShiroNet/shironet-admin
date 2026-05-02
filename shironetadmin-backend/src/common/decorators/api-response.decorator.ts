00bb19f8a4283c074a8a919596849ebdc53f9466
Sakurashiyo <x2821430542@gmail.com>
2026-05-02 15:28:04 +0800

    -

--- shironetadmin-backend/src/common/decorators/api-response.decorator.ts
+++
@@ -1,5 +0,0 @@
-// src/common/decorators/api-response.decorator.ts
-import { SetMetadata } from '@nestjs/common';
-
-export const SKIP_RESPONSE_TRANSFORM = 'skipResponseTransform';
-export const SkipResponseTransform = () => SetMetadata(SKIP_RESPONSE_TRANSFORM, true);
--- shironetadmin-backend/src/redeem/dto/update-redeem-code.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/redeem/dto/update-redeem-code.dto.ts
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRedeemCodeDto } from './create-redeem-code.dto';
-
-export class UpdateRedeemCodeDto extends PartialType(CreateRedeemCodeDto) {}
